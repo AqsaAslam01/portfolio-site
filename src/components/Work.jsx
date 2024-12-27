81951868685cfd9a128b0497bf70961d0c6427fe
AqsaAslam01 <aqsaaslam.78@gmail.com>
2024-12-27 19:21:38 +0500

    modified project containers

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -9,13 +9,13 @@ const Work = () => {
     //setProject(data);
 
     return (
-        <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        <div name='work' className='w-full text-gray-300 bg-[#0a192f]'>
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
                         Work
                     </p>
-                    <p className='py-6'>// Check out some of my recent work</p>
+                    <p className='py-6'>Check out some of my recent work</p>
                 </div>
 
                 {/* container for projects */}
@@ -25,9 +25,9 @@ const Work = () => {
                     {project.map((item, index) => (
                         <div
                             key={index}
-                            style={{ backgroundImage: `url(${item.image})` }}
+                            style={{ backgroundImage: `url(${item.image})`, height: '400px' }}
                             className="shadow-lg shadow-[#040c16] group container rounded-md 
-              flex justify-center text-center items-center mx-auto content-div "
+                                    flex justify-center text-center items-center mx-auto content-div "
                         >
                             {/* Hover effect for images */}
                             <div className="opacity-0 group-hover:opacity-100 ">
@@ -39,20 +39,20 @@ const Work = () => {
                                     <a href={item.github} target="_blank">
                                         <button
                                             className="text-center rounded-lg px-4 py-3 m-2
-                       bg-white text-gray-700 font-bold text-lg"
+                                            bg-white text-gray-700 font-bold text-lg"
                                         >
                                             Code
                                         </button>
                                     </a>
                                     {/* eslint-disable-next-line */}
-                                    <a href={item.live} target="_blank">
+                                    {/* <a href={item.live} target="_blank">
                                         <button
                                             className="text-center rounded-lg px-4 py-3 m-2
-                       bg-white text-gray-700 font-bold text-lg"
+                                             bg-white text-gray-700 font-bold text-lg"
                                         >
                                             Live
                                         </button>
-                                    </a>
+                                    </a> */}
                                 </div>
                             </div>
                         </div>
