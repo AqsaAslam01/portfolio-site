9e4ac6574a35246702c84b79bcbec03e4ee953b8
AqsaAslam01 <aqsaaslam.78@gmail.com>
2025-04-15 20:00:39 +0500

    replace work heading with projects

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
                     exceptional digital experiences. Currently I am focused on building responsive
                     web applications. </p>
                 <div>
-                    <Link to="work" smooth={true} duration={500} offset={-50}>
+                    <Link to="projects" smooth={true} duration={500} offset={-50}>
                         <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">View Work
                             <span className="group-hover:rotate-90 duration-300">
                                 <HiArrowNarrowRight className="ml-3" />
